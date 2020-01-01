06fa64e06d9298c43606bd4335fc8003abfd6247
zw <1622993630@qq.com>
2020-01-01 22:08:06 +0800

    add configuration

--- mall-demo-application/src/main/java/com/zw/malldemo/HelloController.java
+++ mall-demo-application/src/main/java/com/zw/malldemo/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public String hello(){
-        return "hello hello ";
+        return "hello";
     }
 }
