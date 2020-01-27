1a9fb12fb8f52758046b819dd7d17b7666d14dd8
zw <1622993630@qq.com>
2020-01-27 21:55:07 +0800

    PathUtil amend

--- mall-demo-application/src/main/java/com/zw/malldemo/util/PathUtil.java
+++ mall-demo-application/src/main/java/com/zw/malldemo/util/PathUtil.java
@@ -1,22 +1,54 @@
 package com.zw.malldemo.util;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class PathUtil {
     private static String seperator=System.getProperty("file.separator");
+
+    private static String winPath;
+
+    private static String linuxPath;
+
+    private static String shopPath;
+
+    private static String headLinePath;
+
+    @Value("${win.base.path}")
+    public void setWinPath(String winPath) {
+        PathUtil.winPath = winPath;
+    }
+
+    @Value("${linux.base.path}")
+    public void setLinuxPath(String linuxPath) {
+        PathUtil.linuxPath = linuxPath;
+    }
+
+    @Value("${shop.relevant.path}")
+    public  void setShopPath(String shopPath) {
+        PathUtil.shopPath = shopPath;
+    }
+
+    @Value("${headline.relevant.path}")
+    public void setHeadLinePath(String headLinePath) {
+        PathUtil.headLinePath = headLinePath;
+    }
 
     public static String getImageBasePath(){
         String os=System.getProperty("os.name");
         String basePath="";
         if (os.toLowerCase().startsWith("win")){
-            basePath = "D:/webapp/imgDir";
+            basePath = winPath;
         }else {
-            basePath = "/Users/baidu/work/image";
+            basePath = linuxPath;
         }
         basePath=basePath.replace("/",seperator);
         return basePath;
     }
 
     public static String getShopImagePath(long shopId){
-        String imagePath= "/upload/images/item/shop/" + shopId + "/";
+        String imagePath= shopPath + shopId + "/";
         return imagePath.replace("/", seperator);
     }
 }
